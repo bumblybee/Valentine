2584c8eb0c5e51c98b1f149d1510504aef743d10
bumblybee <47286930+bumblybee@users.noreply.github.com>
2020-02-12 16:07:00 -0600

    Delete card.js

--- card.js
+++
@@ -1,5 +0,0 @@
-const card = document.getElementById("card");
-const inside = document.getElementById("inside");
-const back = document.getElementById("back");
-const front = document.getElementById("front");
-const wrapper = document.getElementById("wrapper");
