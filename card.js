21a3503b30e0cd04d39a1c31e13c3c175bf2d29e
Tiffani Hess <hesstjune@gmail.com>
2020-02-12 13:20:20 -0600

    Valentine's Day Card

---
+++ card.js
@@ -0,0 +1,5 @@
+const card = document.getElementById("card");
+const inside = document.getElementById("inside");
+const back = document.getElementById("back");
+const front = document.getElementById("front");
+const wrapper = document.getElementById("wrapper");
